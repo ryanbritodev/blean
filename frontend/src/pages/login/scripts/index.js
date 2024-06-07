7de6af4fc7d8bb0799c8c25b42155e495d655a90
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-06-06 21:30:29 -0300

    novo redirecionamento

--- frontend/src/pages/login/scripts/index.js
+++ frontend/src/pages/login/scripts/index.js
@@ -4,7 +4,7 @@ loginButton.addEventListener("click", () => {
     const password = document.getElementById('password').value;
     if (email === 'fiap@fiap.com' && password === 'fiap123') {
         alert('Login efetuado');
-        window.location.replace("/src/pages/statistcs/index.html");
+        window.location.replace("/blean/frontend/src/pages/statistcs/index.html");
     } else {
         alert('Usuário não encontrado');
         alert('Para logar, use o email: fiap@fiap.com e a senha: fiap123')
